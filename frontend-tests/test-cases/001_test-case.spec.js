aadd264b28072199bcf0e49ad576c81df22c1b00
Prudhvi Raj Mukiri <prudhvi.mukiri@globallogic.com>
2024-11-11 15:57:14 +0530

    fix: structured the test cases

--- frontend-tests/test-cases/001_test-case.spec.js
+++ frontend-tests/test-cases/001_test-case.spec.js
@@ -1,26 +1,7 @@
 const {test} = require('@playwright/test')
-import { DiscoverPage } from '../pages/discover-page';
 import { HomePage } from '../pages/home-page'
 
 test('Accept Cookies check', async ({page})=>{
     const homepage = new HomePage(page)
     await homepage.acceptCookies()
-})
-
-test('Discover link check', async ({page})=>{
-    const homepage = new HomePage(page)
-    await homepage.verifyDiscoverButton()
-})
-
-test('Ready For Delivery link check', async ({page})=>{
-    const homepage = new HomePage(page)
-    await homepage.verifyReadyForDeliveryButton()
-})
-
-
-test('Print the side Menu values in discover page', async ({page})=>{
-    const discoverpage = new DiscoverPage(page)
-    const homepage = new HomePage(page)
-    await homepage.verifyDiscoverButton()
-    await discoverpage.getMenuOption()
 })
