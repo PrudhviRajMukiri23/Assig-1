afb2dd71c47836b5e75c57214a7b576d6d1a1050
Prudhvi Raj Mukiri <prudhvi.mukiri@globallogic.com>
2024-11-12 14:25:48 +0530

    chore added the test case to validate the client validations

--- frontend-tests/pages/test-driver-page.js
+++ frontend-tests/pages/test-driver-page.js
@@ -1,4 +1,5 @@
 const { TEST_DRIVE_URL } = require("../utils/constants")
+const {expect} = require('@playwright/test')
 
 class TestDriver {
     constructor(page) {
@@ -7,6 +8,9 @@ class TestDriver {
         this.carDropdown = "//div[@class='css-1kt83gu']/span"
         this.car1 = "//*[@id='dropdown-field-:Rlb3albn9aj5:-list-1']"
         this.car2 = "//*[@id='dropdown-field-:Rlb3albn9aj5:-list-2']"
+        this.firstName = "//input[@name='firstName']"
+        this.lastName = "//input[@name='lastName']"
+        this.lastNameValidationErrorMessage = "//div[@id='text-field-:R553albn9aj5:-message']"
     }
 
     async selectDifferentValuesInDropdown() {
@@ -14,7 +18,28 @@ class TestDriver {
         await this.page.locator(this.carDropdown).click()
         await this.page.locator(this.car1).click()
         await this.page.locator(this.car2).click()
-        await this.page.waitForTimeout(3000)
+        await this.page.waitForTimeout(2000)
+
+        await this.page.locator(this.firstName).type("hello")
+        await this.page.keyboard.press("Control+A")
+        await this.page.keyboard.press("Control+C")
+        await this.page.keyboard.press("Backspace")
+        await this.page.keyboard.press("Control+v")
+        await this.page.waitForTimeout(2000)
+
+        await this.page.locator(this.lastName).focus()
+        await this.page.keyboard.type("Raj")
+        await this.page.keyboard.down("Shift")
+        for(let i=0;i<=2;i++){
+            await this.page.keyboard.press("ArrowLeft")
+        }
+        await this.page.keyboard.up("Shift")
+        await this.page.keyboard.press("Backspace")
+        await this.page.keyboard.press("Enter")
+        await this.page.waitForTimeout(2000)
+
+        await expect(await this.page.locator(this.lastNameValidationErrorMessage)).toBeVisible();
+
     }
 }
 
--- frontend-tests/test-cases/005_test-case.spec.js
+++ frontend-tests/test-cases/005_test-case.spec.js
@@ -2,7 +2,7 @@ const {test, expect} = require('@playwright/test')
 const { TestDriver } = require('../pages/test-driver-page');
 const { HomePage } = require('../pages/home-page');
 
-test('select the dropdown value for test drive', async ({page})=>{
+test('select the dropdown value, first name & validate client error', async ({page})=>{
     let testDrive = new TestDriver(page);
     let homepage = new HomePage(page);
     await homepage.acceptCookies()
